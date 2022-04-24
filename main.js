7981de92892aec1f45460ebfbcfd149cdcfccb9d
Guany <1084072690@qq.com>
2022-04-24 13:42:40 +0800

    update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, Menu, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const { autoUpdater } = require('electron-updater')
 
